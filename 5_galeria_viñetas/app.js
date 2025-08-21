f4ab759a8eb96ca8618e572bcc6efa9c52c0fcf1
alexandra Sanchez <alexamolina901@gmail.com>
2025-08-21 15:48:22 -0500

    cambio de colores

--- 5_galeria_viñetas/app.js
+++ 5_galeria_viñetas/app.js
@@ -197,3 +197,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     actualizar();
 });
+//siguiente función boton//
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const escena1 = document.querySelector(".escena-1");
+const escena2 = document.querySelector(".escena-2");
+const escena3 = document.querySelector(".escena-3");
+let i = 1
+
+next.addEventListener("click", function () {
+    if (i === 1){
+        escena1.classList.add('d-none');
+        escena2.classList.remove('d-none');
+        i++
+    } else if (i == 2) {
+        escena2.classList.add('d-none');
+        escena3.classList.remove('d-none');
+        i++
+    }
+})
